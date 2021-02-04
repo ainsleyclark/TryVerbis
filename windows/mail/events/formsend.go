e79ab4e305de55243a19b2bb4256dec706901795
Ainsley Clark <info@ainsleyclark.com>
2021-02-04 14:17:01 +0000

    Code injection fix

--- windows/mail/events/formsend.go
+++ windows/mail/events/formsend.go
@@ -1,8 +1,13 @@
+// Copyright 2020 The Verbis Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package events
 
 import (
 	"github.com/ainsleyclark/verbis/api/config"
 	"github.com/ainsleyclark/verbis/api/domain"
+	"github.com/ainsleyclark/verbis/api/forms"
 	"github.com/ainsleyclark/verbis/api/mail"
 )
 
@@ -28,18 +33,19 @@ func NewFormSend(config config.Configuration) (*FormSend, error) {
 }
 
 // Send the verify email event.
-func (e *FormSend) Send(form *domain.Form) error {
+func (e *FormSend) Send(form *domain.Form, values forms.FormValues, attachments forms.Attachments) error {
 	const op = "events.VerifyEmail.Send"
 
-	html, err := e.mailer.ExecuteHTML("form-send.html", nil)
+	html, err := e.mailer.ExecuteHTML("form-send.html", values)
 	if err != nil {
 		return err
 	}
 
 	tm := mail.Sender{
-		To:      []string{"ainsley@reddico.co.uk"},
-		Subject: form.EmailSubject,
-		HTML:    html,
+		To:          []string{"ainsley@reddico.co.uk"},
+		Subject:     form.EmailSubject,
+		HTML:        html,
+		Attachments: attachments,
 	}
 
 	e.mailer.Send(&tm)
